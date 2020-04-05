b42a42219be1e8da5c74735333415f5f02c461a2
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2020-04-05 17:00:40 -0400

    mail works locally now testing live server

--- data/contact.php
+++ data/contact.php
@@ -1,7 +1,5 @@
 <?php
 
-var_dump($_POST['name']);
-
 if(empty($_POST)){
     echo 'You forgot a field?';
     exit;
@@ -10,11 +8,14 @@ if(empty($_POST)){
 // Validate all data
 $name = '';
 $email = '';
+$subject = 'mail from portfolio';
+$message = '';
 $recipient = 'daffodilho@gmail.com';
 
 if(isset($_POST['name'])){
     $name = filter_var($_POST['name'],FILTER_SANITIZE_STRING);
 }
+
 if(isset($_POST['email'])){
     $email = str_replace(array("\r", "\n", "%0a", "%0d"), '',$_POST['email']);
     $email = filter_var($email,FILTER_VALIDATE_EMAIL);
@@ -27,10 +28,14 @@ if(isset($_POST['message'])){
 // Send out email
 $headers = array(
     'FROM'=>'noreply@test.ca',
-    'Reply-To'=>$name.'<'.$email.'>'
+    'Reply-To'=> ".$name.'<'.$email.'>'."
 );
 
-if(mail($recipient, $message, $headers)){
+echo $message;
+echo $recipient;
+var_dump ($headers);
+
+if(mail($recipient, $subject, $message, $headers)){
     echo '<p>Thank you for contacting us, '.$name.'</p>';
 }else{
     echo '<p>We are sorry but the email did not go through</p>';
