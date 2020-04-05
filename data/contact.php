4d4d7ff73d9cacc789df0c348bd6392b0e4b7498
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2020-04-05 16:44:15 -0400

    changed some contact form stuff

--- data/contact.php
+++ data/contact.php
@@ -10,8 +10,6 @@ if(empty($_POST)){
 // Validate all data
 $name = '';
 $email = '';
-$subject = '';
-$message = '';
 $recipient = 'daffodilho@gmail.com';
 
 if(isset($_POST['name'])){
@@ -20,10 +18,6 @@ if(isset($_POST['name'])){
 if(isset($_POST['email'])){
     $email = str_replace(array("\r", "\n", "%0a", "%0d"), '',$_POST['email']);
     $email = filter_var($email,FILTER_VALIDATE_EMAIL);
-}
-
-if(isset($_POST['subject'])){
-    $subject = filter_var($_POST['subject'],FILTER_SANITIZE_STRING);
 }
 
 if(isset($_POST['message'])){
@@ -36,7 +30,7 @@ $headers = array(
     'Reply-To'=>$name.'<'.$email.'>'
 );
 
-if(mail($recipient, $subject, $message, $headers)){
+if(mail($recipient, $message, $headers)){
     echo '<p>Thank you for contacting us, '.$name.'</p>';
 }else{
     echo '<p>We are sorry but the email did not go through</p>';
