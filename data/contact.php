a034bd09b3763da58980fd65a4bdf8fae4ea35c9
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2020-04-06 11:33:39 -0400

    testing sendgrid

--- data/contact.php
+++ data/contact.php
@@ -5,39 +5,25 @@
 //     exit;
 // }
 
-// // If you are not using Composer (recommended)
+// If you are using Composer
+require 'vendor/autoload.php';
+
+// If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-// $request_body = json_decode('{
-//   "personalizations": [
-//     {
-//       "to": [
-//         {
-//           "email": "test@example.com"
-//         }
-//       ],
-//       "subject": "Hello World from the SendGrid PHP Library!"
-//     }
-//   ],
-//   "from": {
-//     "email": "test@example.com"
-//   },
-//   "content": [
-//     {
-//       "type": "text/plain",
-//       "value": "Hello, Email!"
-//     }
-//   ]
-// }');
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "test@example.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-// $apiKey = getenv('SENDGRID_API_KEY');
-// $sg = new \SendGrid($apiKey);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
 
-// $response = $sg->client->mail()->send()->post($request_body);
-// echo $response->statusCode();
-// echo $response->body();
-// echo $response->headers();
-
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
 // // Validate all data
 // $name = '';
