f075ce923f0774b5a9fa9d40acf035dee5fce34a
Daffodil Ho <daffodilho@Daffodils-MacBook-Pro.local>
2020-04-06 12:47:15 -0400

    changing mail stuff again

--- data/contact.php
+++ data/contact.php
@@ -11,9 +11,9 @@ require 'vendor/autoload.php';
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "test@example.com");
+$from = new SendGrid\Email(null, "daffodilho.com");
 $subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "test@example.com");
+$to = new SendGrid\Email(null, "daffodilho@gmail.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
